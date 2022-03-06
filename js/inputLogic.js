a472203841d111cc4346a6c3214e06d3edc317fa
Sergey <makarkapar@gmail.com>
2022-03-06 13:23:40 +0300

    First commit

--- js/inputLogic.js
+++ js/inputLogic.js
@@ -1,7 +1,25 @@
-button.onclick = function() {
-    let a = Number(document.getElementById('input-x1').value);
-    let b = Number(document.getElementById('input-x2').value);
-    let c = Number(document.getElementById('input-x3').value);
-    let result = c * (b - a);
-    document.getElementById('str').innerHTML = "Результат: "+result;
-};
+$("#input-x1").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $("#button").click();
+    }
+});
+$("#input-x2").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $("#button").click();
+    }
+});
+$("#input-x3").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $("#button").click();
+    }
+});
+
+$("#button").click(function() {
+    button.onclick = function() {
+        let a = Number(document.getElementById('input-x1').value);
+        let b = Number(document.getElementById('input-x2').value);
+        let c = Number(document.getElementById('input-x3').value);
+        let result = c * (b - a);
+        document.getElementById('str').innerHTML = "Результат: "+result;
+    };
+});
