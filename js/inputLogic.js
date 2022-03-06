bfe510cf34d316b70fff2aa22accd586cbfb5918
Sergey <makarkapar@gmail.com>
2022-03-06 13:08:03 +0300

    commit commit

--- js/inputLogic.js
+++ js/inputLogic.js
@@ -3,5 +3,5 @@ button.onclick = function() {
     let b = Number(document.getElementById('input-x2').value);
     let c = Number(document.getElementById('input-x3').value);
     let result = c * (b - a);
-    document.getElementById('str').innerHTML = result;
+    document.getElementById('str').innerHTML = "Результат: "+result;
 };
